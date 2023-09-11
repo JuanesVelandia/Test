190f67d129129423da928a76568008f2a5defcd9
JuanesVelandia <101300592+JuanesVelandia@users.noreply.github.com>
2023-09-11 16:05:35 -0500

    Delete myproject directory

--- myproject/myapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.subir_archivo, name='subir_archivo'),
-    path('descargar/<int:archivo_id>/', views.descargar_archivo, name='descargar_archivo'),
-    path('borrar_registros/', views.borrar_registros, name='borrar_registros'),
-    path('information/', views.information ,name="information")
-]
